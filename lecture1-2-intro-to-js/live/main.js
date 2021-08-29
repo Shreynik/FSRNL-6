2b39a5223cc8bdc01ddda51bfd21a999b9426e18
pjosephraj <mailjosephraj@gmail.com>
2021-08-29 16:06:39 +0530

    Add book functionality added...

--- lecture1-2-intro-to-js/live/main.js
+++ lecture1-2-intro-to-js/live/main.js
@@ -1,56 +1,83 @@
 
-(function test(){
+(function test() {
 
-    const initalBooks = [
-        {
-            id: '1',
-            name:'book1',
-            lender: 'shreynik',
-            borrower: null
-        },
-        {
-            id: '2',
-            name:'book2',
-            lender: 'ssdfsdf',
-            borrower: 'shreynik'
-        },
-        {
-            id: '3',
-            name:'book3',
-            lender: 'sdsfafsdf',
-            borrower: null
-        }
-    ];
+	const userInpEl = document.querySelector('#user-input');
+	const userSubmitEl = document.querySelector('#user-submit-btn');
 
-    class Store{
+	const initalBooks = [
+		{
+			id: '1',
+			name: 'book1',
+			lender: 'shreynik',
+			borrower: null
+		},
+		{
+			id: '2',
+			name: 'book2',
+			lender: 'ssdfsdf',
+			borrower: 'shreynik'
+		},
+		{
+			id: '3',
+			name: 'book3',
+			lender: 'sdsfafsdf',
+			borrower: null
+		}
+	];
 
-        constructor(list){
-            this.bookList = list
-        }
+	class Store {
 
-        render(){
-            // render the books
-            showTable(this.bookList)
-        }
+		constructor(list) {
+			this.bookList = list;
+			this.lastId = list.length;
+		}
 
-        handleLogin(){
+		render() {
+			// render the books
+			showTable(this.bookList);
+			userSubmitEl.addEventListener('click', this.handleLogin.bind(this));
+		}
 
-        }
+		handleLogin() {
+			const userName = userInpEl.value;
+			if (userName) {
+				userSubmitEl.disabled = true;
+				this.user = userName;
+				const book = {
+					id: '',
+					name: '',
+					lender: this.user,
+					borrower: null,
+					action: ''
+				};
+				this.lastId++;
+				book.id = this.lastId;
+				addRow.bind(this)(book);
+			}
+		}
 
-        handleActions(){
+		handleActions() {
 
-        }
+		}
 
 
-        addBook(){
-            
-        }
+		addBook(book, trEleToRemove) {
+			const { id, name, lender } = book;
+			const bookToSave = {
+				id, name, lender,
+				borrower: '-', action: 'Book Added'
+			};
+			trEleToRemove.remove();
+			this.bookList.push(bookToSave);
+			showTable([bookToSave]);
+			userSubmitEl.disabled = false;
+		}
 
-    }
+	}
 
-    document.body.onload = function(){
-        const myStore = new Store(initalBooks);
-        myStore.render();
-    }
+	document.body.onload = function () {
+		const myStore = new Store(initalBooks);
+		myStore.render();
+	}
 
 })();
--- lecture1-2-intro-to-js/live/renderTable.js
+++ lecture1-2-intro-to-js/live/renderTable.js
@@ -1,17 +1,40 @@
-function showTable(listOfBooks){
-    const tableEle = document.getElementsByTagName('table')[0];
-    tableEle.style.border="1px solid red"
-    listOfBooks.forEach(book => {
-        const trEle = document.createElement('tr');
-        const allTds = [];
-        for(let key in book){
-            const tdEle = document.createElement('td');
-            tdEle.innerHTML = book[key];
-            allTds.push(tdEle);
-        }
-        allTds.forEach(td=>{
-            trEle.appendChild(td);
-        })
-        tableEle.appendChild(trEle);
-    });
+const tableEle = document.querySelector('.table-tbody');
+
+function showTable(listOfBooks) {
+	listOfBooks.forEach(book => {
+		const trEle = document.createElement('tr');
+		for (let key in book) {
+			const tdEle = document.createElement('td');
+			tdEle.innerHTML = book[key];
+			trEle.appendChild(tdEle);
+		}
+		tableEle.appendChild(trEle);
+	});
+}
+
+function addRow(book) {
+	const trEle = document.createElement('tr');
+	let inpName;
+	for (let key in book) {
+		const tdEle = document.createElement('td');
+		if (key === 'name') {
+			inpName = document.createElement('input');
+			inpName.placeholder = 'Enter book name';
+			inpName.className = 'inp-book-name';
+			tdEle.appendChild(inpName);
+		} else if (key === 'action') {
+			const trBtn = document.createElement('button');
+			trBtn.className = 'inp-add-btn'
+			trBtn.innerHTML = 'Add the Book';
+			trBtn.addEventListener('click', () => {
+				book.name = inpName.value;
+				this.addBook(book, trEle);
+			});
+			tdEle.appendChild(trBtn);
+		} else {
+			tdEle.innerHTML = book[key];
+		}
+		trEle.appendChild(tdEle);
+	}
+	tableEle.appendChild(trEle);
 }
